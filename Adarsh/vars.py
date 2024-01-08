31097281884850f1e61a67e33cf0f2539cc2d95b
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2024-01-08 12:36:44 +0530

    Add files via upload

---
+++ Adarsh/vars.py
@@ -0,0 +1,43 @@
+# (c) adarsh-goel
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', '27116256'))
+    API_HASH = str(getenv('API_HASH', '89669f3ecf7d5257926a50701371100b'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6327998819:AAH0aaiuU0C9729mKDczT7OLDsq2IxwUsqM'))
+    name = str(getenv('name', 'Fast_Downloader_bot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001841580553'))
+    PORT = int(getenv('PORT', '8080'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", " 5002159457").split())  
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Sivam_uv'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME'))
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Anshu01:Anshu01@cluster0.vhtlpsp.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'newmoviesot'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))      
+    SHORTLINK_URL = getenv('SHORTLINK_URL', 'onepagelink.in')
+    SHORTLINK_API = getenv('SHORTLINK_API', '6b008bca8d172da3eac6261ad32929296d227e49')
+    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/rk_back_up/18')
