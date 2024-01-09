8bd7e0a7e7af1413ac0e0c1a4e52f669c2b070ac
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2024-01-09 08:38:42 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -30,7 +30,7 @@ def generate_random_alphanumeric():
     return random_chars 
   
 def get_shortlink(url): 
-    rget = requests.get(f"https://{Var.SHORTLINK_URL}/api?api={Var.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}") 
+    rget = requests.get(f"https://{Var.SHORTLINK_URL}/easy_api?api={Var.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}") 
     rjson = rget.json() 
     if rjson["status"] == "success" or rget.status_code == 200: 
         return rjson["shortenedUrl"] 
