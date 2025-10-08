3699a486b3b5d6c51c287a48e911700b90a6ce1b
owajbhai <owajshaikhrcm@gmail.com>
2025-10-08 13:51:37 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '27116256'))
-    API_HASH = str(getenv('API_HASH', '89669f3ecf7d5257926a50701371100b'))
+    API_ID = int(getenv('API_ID', '23621595'))
+    API_HASH = str(getenv('API_HASH', 'de904be2b4cd4efe2ea728ded17ca77d'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6620784685:AAGLKiPLBWNlnBBEwcOSL-SZV-TEl1g_0yQ'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
@@ -17,10 +17,10 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5002159457").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1249672673").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Sivam_uv'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'botmaster55'))
 
     if 'DYNO' in environ:
         ON_HEROKU = True
@@ -36,9 +36,9 @@ class Var(object):
     else:
         URL = "https://advancestreambot-11kq.onrender.com/".format(FQDN)
 
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Marvelbotz:Marvelbotz@cluster0.oyxdumi.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'marvelbackupp'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://georgarevenio:8Gh0DpkI4K3RkVPA@cluster0.gyezl2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'ClipMateBhai'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
-    SHORTLINK_URL = getenv('SHORTLINK_URL', 'linkcents.com')
-    SHORTLINK_API = getenv('SHORTLINK_API', '84a3e44eb56e270b5f8eb969e1f13d8098b2a60d')
-    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/marvelsbackup/3')
+    SHORTLINK_URL = getenv('SHORTLINK_URL', '')
+    SHORTLINK_API = getenv('SHORTLINK_API', '')
+    TUTORIAL_URL = getenv('TUTORIAL_URL', '')
